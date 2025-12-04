352b3f8a0dde9d6d4bff6ff8077702a60c796bad
Dina Stepanchuk <stepanchukdin23@gmail.com>
2025-12-04 22:58:18 +0200

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,8 @@
+const categories = document.querySelectorAll('#categories .item');
+console.log(`Number of categories ${categories.length}`);
+categories.forEach(item => {
+  const title = item.querySelector('h2').textContent;
+  const numberCategories = item.querySelectorAll('li').length;
+  console.log(`Category: ${title}`);
+  console.log(`Elements: ${numberCategories}`);
+});
